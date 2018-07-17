037d58ce22f84a64ada8b49c38a9bf67763412cb
Juan Andres Moreno <amorenocb@gmail.com>
2018-07-17 10:31:25 -0400

    Added projects page

--- server.js
+++ server.js
@@ -42,6 +42,13 @@ app.get('/', (req, res) => {
   })
 });
 
+app.get('/projects', (req,res) => {
+  res.render('projects.hbs', {
+    pageTitle : 'Projects',
+    welcomeMessage: 'Here you can see all my projects.'
+  })
+})
+
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle : 'About Page',
